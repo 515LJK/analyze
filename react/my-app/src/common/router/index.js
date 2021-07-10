2d37e6807c4342a9342f0e9af95f93385d6b96cf
515LJK <936658663@qq.com>
2021-07-10 10:10:46 +0800

    feat: react

--- react/my-app/src/common/router/index.js
+++ react/my-app/src/common/router/index.js
@@ -3,6 +3,8 @@ import {HashRouter, Route, Switch} from 'react-router-dom';
 import Hook from '../../components/hook/hook';
 import App from '../../components/app/app';
 import User from '../../components/redux/index';
+import List from '../../components/router/list';
+import Users from '../../components/router/user';
 
 const router = ()=> (
     <HashRouter>
@@ -10,6 +12,8 @@ const router = ()=> (
             <Route exact path="/" component={App}/>
             <Route exact path="/hook" component={Hook}/>
             <Route path="/user" component={User} />
+            <Route path="/list" component={List} />
+            <Route path="/users/:id" component={Users} />
         </Switch>
     </HashRouter>
 )
